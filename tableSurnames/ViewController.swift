b337c69eea298273ca42900d95977af75506d023
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-05-23 11:17:59 +0200

    first commit

--- tableSurnames/ViewController.swift
+++ tableSurnames/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var surname = ["Kubheka ", "Khathide ", "Buthelezi ", "Shenge ", "Mahlangu ", "Toyi ", "Sekhukhune ", "Morapeli ", "Kubheka ", "Khathide ", "Buthelezi ", "Shenge ", "Mahlangu ", "Toyi ", "Sekhukhune ", "Morapeli "]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return surname.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = surname[indexPath.row]
+        return cell
+    }
+    
+    
+}
